874f27f3ed4dabbcd7a9988f621b6944e8d53cc5
Adrián García Herrera <andruxand@gmail.com>
2018-04-12 15:41:08 -0500

    control de errores a DB
    se crea variable y función para capturar errores en consultas y
    transacciones con la DB con la posibilidad de mostrarse como un mensaje
    controldo en la UI

--- app/libs/BaseDatos.php
+++ app/libs/BaseDatos.php
@@ -6,6 +6,7 @@ class BaseDatos
 {
     protected $conexion;
     protected $db;
+    public $db_error = '';
 
     public function conectar()
     {
@@ -21,8 +22,8 @@ class BaseDatos
 
     public function desconectar()
     {
-        if ($this->conectar->conexion) {
-            mysqli_close($this->$conexion);
+        if ($this->conexion) {
+            mysqli_close($this->conexion);
         }
 
     }
@@ -31,12 +32,18 @@ class BaseDatos
     function query_exec($query) {
         if($this->conectar()){
             $result = mysqli_query($this->conexion, $query);
+
+            if(!$result){
+                $this->db_error = mysqli_error($this->conexion);
+            }
+
+            $this->desconectar();
             return $result;
         }
     }
 
     //Función Insertar
-     function insert($tblname, $form_data){
+    function insert($tblname, $form_data){
         $fields = array_keys($form_data);
         $sql = "INSERT INTO " . $tblname . "(" . implode(',', $fields) . ")  VALUES('" . implode("','", $form_data) . "')";
         
@@ -44,8 +51,15 @@ class BaseDatos
     }
 
     //Función para Select y update
-     function sql_exec($query){    
+    function sql_exec($query){    
         return $this->query_exec($query);
+    }
+
+    function show_db_error(){
+        return  "<div class='alert alert-danger' role='alert'>" .
+                " <span class='oi oi-circle-x' title='icon name' aria-hidden='true'></span>" .
+                " Error al consultar la base de datos: " . $this->db_error .
+                "</div>";
     }
 
 }
