a47764b34cd0ea1145c3dfce6a516e5b677eb883
Adrián García Herrera <andruxand@gmail.com>
2018-04-16 16:17:45 -0500

    instancia datatables
    se crea las opciones para realizar la petición ajax y cargar los datos
    gracias al archivo ajax.php y el uso de datatables

--- app/modules/alimentario/js/script.js
+++ app/modules/alimentario/js/script.js
@@ -1,5 +1,81 @@
 $(document).ready(function() {
 
-	$('#institucion').select2();
+	var institucion = $('#institucion');
+
+	institucion.select2();
+
+	CargaResutlados('no');
+
+	function CargaResutlados(is_custom_search, institucion = ''){
+
+		var dataTable = $('#results').DataTable( {
+			"processing": true,
+			"serverSide": true,
+			//"sort": false,
+   			//"order" : [],
+	        "ajax": {
+	        	url: "ajax.php",
+	        	type: "post",
+	        	data: { is_custom_search: is_custom_search, institucion: institucion },
+	        	error: function(e){  // error handling
+					console.log(e);				
+				}
+	        },
+	        "columns": [
+	            { "data": "dane" },
+	            { "data": "proveedor" },
+	            { "data": "institucion" },
+	            { "data": "direccion" },
+	            { "data": "comuna" },
+	            { "data": "tipo_zona" },
+	            { "data": "sector" },
+	            { "data": "zona" },
+	            { "data": "modalidad" },
+	            { "data": "formacion" },
+	            { "data": "acciones" }
+	        ],
+	        "language": {
+					"emptyTable":			"<div class='alert alert-warning alert-dismissible fade show animated bounceInDown' role='alert'>" +
+                							"<span class='oi oi-warning' title='icon name' aria-hidden='true'></span>" +
+                							"Lo sentimos!. No se encontraron registros que cumplan los criterios de búsqueda especificados.</div>",
+					"info":		   			"Del _START_ al _END_ de _TOTAL_ ",
+					"infoEmpty":			"Mostrando 0 registros de un total de 0.",
+					"infoFiltered":			"(filtrados de un total de _MAX_ registros)",
+					"infoPostFix":			"(actualizados)",
+					"lengthMenu":			"Mostrar _MENU_ registros",
+					"loadingRecords":		"Cargando...",
+					"processing":			"Cargando...",
+					"search":				"Buscar:",
+					"searchPlaceholder":	"Búsqueda Global",
+					"zeroRecords":			"No se han encontrado coincidencias.",
+					"paginate": {
+						"first":			"Primera",
+						"last":				"Última",
+						"next":				"Siguiente",
+						"previous":			"Anterior"
+					},
+					"aria": {
+						"sortAscending":	"Ordenación ascendente",
+						"sortDescending":	"Ordenación descendente"
+					}
+				},
+	    } );
+
+	}
+
+	$('#buscar').click(function(){
+		
+		$('#results').DataTable().destroy();
+		CargaResutlados('yes', institucion.val());
+
+	});
+
+	$('#reset').click(function(){
+		
+		$("#results").DataTable().destroy();
+		CargaResutlados('no');
+		institucion.val('').trigger('change.select2');
+
+	});
 
 });
